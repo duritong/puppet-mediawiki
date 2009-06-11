ca076a34d669332f3380c54fedbfa941cfee0825
duritong <peter.meier@immerda.ch>
2009-06-11 08:47:11 +0200

    added dependencies

--- manifests/base.pp
+++ manifests/base.pp
@@ -3,9 +3,13 @@ class mediawiki::base {
     ensure => present,
   }
 
+  include rubygems::highline
+  include ruby::mysql
+
   file{'/opt/bin/mediawiki_dbupdate.rb':
     source => [ "puppet://$server/files/mediawiki/updatescript/mediawiki_dbupdate.rb",
                 "puppet://$server/mediawiki/updatescript/mediawiki_dbupdate.rb" ],
+    require => [ Package['ruby-mysql'], Package['rubygems-highline'] ],
     owner => root, group => 0, mode => 0700;
   }
 }
