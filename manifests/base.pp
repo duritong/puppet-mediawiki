4e5d874ea2f47995e0668e69c9a6db11f7673de9
mh <mh@immerda.ch>
2009-11-02 00:50:39 +0100

    update file path to new 0.25 style

--- manifests/base.pp
+++ manifests/base.pp
@@ -9,12 +9,12 @@ class mediawiki::base {
   file{'/opt/bin/mediawiki_dbupdate.config.rb':
     source => [ "puppet://$server/files/mediawiki/updatescript/${fqdn}/mediawiki_dbupdate.config.rb",
                 "puppet://$server/files/mediawiki/updatescript/mediawiki_dbupdate.config.rb",
-                "puppet://$server/mediawiki/updatescript/mediawiki_dbupdate.config.rb" ],
+                "puppet://$server/modules/mediawiki/updatescript/mediawiki_dbupdate.config.rb" ],
     require => [ Package['ruby-mysql'], Package['rubygem-highline'] ],
     owner => root, group => 0, mode => 0600;
   }
   file{'/opt/bin/mediawiki_dbupdate.rb':
-    source => "puppet://$server/mediawiki/updatescript/mediawiki_dbupdate.rb",
+    source => "puppet://$server/modules/mediawiki/updatescript/mediawiki_dbupdate.rb",
     require => File['/opt/bin/mediawiki_dbupdate.config.rb'],
     owner => root, group => 0, mode => 0700;
   }
