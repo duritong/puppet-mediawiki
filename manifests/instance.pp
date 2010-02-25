0059ffe67add8c996c98eb2ff2d6fcfb261f0986
mh <mh@immerda.ch>
2010-02-25 17:19:44 +0100

    link correct files

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -78,11 +78,11 @@ define mediawiki::instance(
     }
 
     mediawiki::file{
-      [ "${real_path}/languages", "${real_path}/php5.php5", "${real_path}/thumb.php", "${real_path}/img_auth.php",
+      [ "${real_path}/languages", "${real_path}/php5.php", "${real_path}/thumb.php", "${real_path}/img_auth.php",
         "${real_path}/redirect.php", "${real_path}/trackback.php", "${real_path}/includes", "${real_path}/redirect.phtml",
         "${real_path}/wiki.phtml", "${real_path}/index.php", "${real_path}/math", "${real_path}/skins",
-        "${real_path}/extensions", "${real_path}/install-utils.inc", "${real_path}/opensearch_desc.php5",
-        "${real_path}/serialized", "${real_path}/StartProfiler.php", "${real_path}/trackback.php5" ]:
+        "${real_path}/extensions", "${real_path}/install-utils.inc", "${real_path}/opensearch_desc.php",
+        "${real_path}/serialized", "${real_path}/StartProfiler.php", "${real_path}/trackback.php" ]:
       src_path => $basedir,
     }
 
