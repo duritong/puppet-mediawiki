31afa33e9bb521e7a10aaed568b216082fa5f5c2
mh <mh@immerda.ch>
2012-05-13 19:14:55 +0200

    fix typo

--- manifests/math.pp
+++ manifests/math.pp
@@ -10,7 +10,7 @@ class mediawiki::math {
       command => 'make',
       cwd => '/var/www/mediawiki/extensions/Math/math/',
       creates => '/var/www/mediawiki/extensions/Math/math/texvc',
-      requires => Git::Clone['mediawiki'];
+      require => Git::Clone['mediawiki'];
     'generalte_latex.fmt':
       command => '/var/www/mediawiki/extensions/Math/math/texvc /tmp /tmp "y=x+2"',
       creates => '/root/.texmf-var/web2c/latex.fmt';
